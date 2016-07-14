f7f7aeed02c38d84c62d40cc8a9d79d7b86fc9a9
Waters <h2oboi89@gmail.com>
2016-07-14 18:20:52 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var _jasmine = require('gulp-jasmine');
+var istanbul = require('gulp-istanbul');
+var tap = require('gulp-tap');
+
+var source = ['./src/**/*.js', './index.js'];
+var tests = ['./spec/**/*.js'];
+var gulpfile = ['./gulpfile.js'];
+var all = [].concat(source).concat(tests).concat(gulpfile);
+
+var jasmineConfig = {
+  'spec_dir': 'spec',
+  'spec_files': ['**/*[sS]pec.js']
+};
+
+gulp.task('test', function() {
+  return gulp.src([].concat(tests))
+    .pipe(_jasmine({
+      config: jasmineConfig,
+      includeStackTrace: true
+    }));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(all)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('coverage', function() {
+  return gulp.src([].concat(source))
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .pipe(tap(function(f) {
+      require(f.path);
+    }))
+    .on('end', function() {
+      gulp.src([].concat(tests))
+        .pipe(_jasmine({
+          config: jasmineConfig
+        }))
+        .pipe(istanbul.writeReports({
+          dir: './coverage',
+          reporters: ['text', 'text-summary', 'lcov'],
+        }));
+    });
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['lint'], ['test']);
+});
