dd09bc8123eee9f14677b2dd4e9827f4bc3c9e5d
Waters <h2oboi89@gmail.com>
2016-07-14 18:38:37 -0400

    Initial implementation ported from async-loops

---
+++ src/utilities.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * Wrapper for tests that should reject.
+ * @function
+ * @param {function} thunk - Code to execute that should Promise.reject
+ * @param {string|Error} reason - Expected value that thunk should reject with
+ * @returns {Promise} Resolves after checking expectations.
+ */
+let shouldReject = (thunk, reason) => {
+  return thunk
+    .then(() => fail('should have rejected'))
+    .catch((error) => expect(error).toEqual(reason));
+};
+
+/**
+ * Wrapper for tests that should resolve.
+ * @function
+ * @param {function} thunk - Code to execute that should Promise.resolve
+ * @param {object} reason - Expected value that thunk should resolve with
+ * @returns {Promise} Resolves after checking expectations.
+ */
+let shouldResolve = (thunk, expectedValue) => {
+  return thunk.then((value) => expect(value).toEqual(expectedValue))
+};
+
+/**
+ * Will automatically wrap test in a `catch` block to catch any errors
+ * and call `done` afterwards.
+ * Use this in place of `(done) => { <TEST> }`.
+ * @function
+ * @param {function} thunk - asynchronous test
+ * @returns {Promise} Resolves after running test
+ */
+let asyncTest = (thunk) => (done) => {
+  return thunk()
+    .catch(fail)
+    .then(done);
+};
+
+module.exports = {
+  asyncTest: asyncTest,
+  shouldReject: shouldReject,
+  shouldResolve: shouldResolve
+};
