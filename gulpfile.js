982d0c934008c8f9c8b01b2ca86232f9cbdaba22
Waters <daniel.waters2@ge.com>
2016-09-26 16:58:21 -0400

    Major overhaul
     - added support for #3

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,67 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var _jasmine = require('gulp-jasmine');
-var istanbul = require('gulp-istanbul');
-var tap = require('gulp-tap');
+'use strict';
 
-var source = ['./src/**/*.js', './index.js'];
-var tests = ['./spec/**/*.js'];
-var gulpfile = ['./gulpfile.js'];
-var all = [].concat(source).concat(tests).concat(gulpfile);
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const _jasmine = require('gulp-jasmine');
+const JasmineConsoleReporter = require('jasmine-console-reporter');
+const istanbul = require('gulp-istanbul');
+const runSequence = require('run-sequence');
+const tap = require('gulp-tap');
 
-var jasmineConfig = {
+const source = ['./src/**/*.js', './index.js'];
+const sample = ['./sample/**/*.js'];
+const tests = ['./spec/**/*.js'];
+const gulpfile = ['./gulpfile.js'];
+const all = [].concat(source).concat(sample).concat(tests).concat(gulpfile);
+
+const jasmineConfig = {
   'spec_dir': 'spec',
   'spec_files': ['**/*[sS]pec.js']
 };
 
-gulp.task('test', function() {
-  return gulp.src([].concat(tests))
+const WATCH_VERBOSITY = 1;
+const TEST_VERBOSITY = 4;
+
+let verbosity = TEST_VERBOSITY;
+
+gulp.task('setup-istanbul', () => {
+  return gulp.src(source)
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('require-all', ['setup-istanbul'], () => {
+  return gulp.src(source)
+    .pipe(tap((f) => require(f.path)));
+});
+
+gulp.task('test', ['require-all'], () => {
+  console.log('testing...');
+  return gulp.src(tests)
     .pipe(_jasmine({
       config: jasmineConfig,
+      reporter: new JasmineConsoleReporter({
+        verbosity: verbosity
+      }),
       includeStackTrace: true
+    }))
+    .pipe(istanbul.writeReports({
+      dir: './coverage',
+      reporters: ['text', 'text-summary', 'lcov'],
     }));
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', () => {
   return gulp.src(all)
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('coverage', function() {
-  return gulp.src([].concat(source))
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire())
-    .pipe(tap(function(f) {
-      require(f.path);
-    }))
-    .on('end', function() {
-      gulp.src([].concat(tests))
-        .pipe(_jasmine({
-          config: jasmineConfig
-        }))
-        .pipe(istanbul.writeReports({
-          dir: './coverage',
-          reporters: ['text', 'text-summary', 'lcov'],
-        }));
-    });
+gulp.task('lite', () => {
+  verbosity = WATCH_VERBOSITY;
+  runSequence('lint', 'test');
 });
 
-gulp.task('watch', function() {
-  gulp.watch(['lint'], ['test']);
+gulp.task('default', () => {
+  runSequence('lint', 'test');
 });
